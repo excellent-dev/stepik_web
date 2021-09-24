67a8572295bedb35b78f5ac6dab1c67460befa1b
excellent-dev <62356925+excellent-dev@users.noreply.github.com>
2021-09-24 15:54:27 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
+# №1
 #sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 #sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+# №2
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
+# sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo /etc/init.d/gunicorn restart
